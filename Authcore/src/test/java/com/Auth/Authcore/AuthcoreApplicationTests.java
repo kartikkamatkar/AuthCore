e0e613cd3126a1b9f8d9e9d237139ab436f56861
Kartik Kamatkar <KartikKamatkar20@gmail.com>
2026-04-16 14:34:05 +0530

    first commit

---
+++ Authcore/src/test/java/com/Auth/Authcore/AuthcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Auth.Authcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
