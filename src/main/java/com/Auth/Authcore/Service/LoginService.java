5fc026aae5e9dd46962733b22a08fd572a60e3d7
Kartik Kamatkar <KartikKamatkar20@gmail.com>
2026-05-21 10:40:05 +0530

    change in repo

--- src/main/java/com/Auth/Authcore/Service/LoginService.java
+++ src/main/java/com/Auth/Authcore/Service/LoginService.java
@@ -1,5 +1,22 @@
 package com.Auth.Authcore.Service;
 
+import com.Auth.Authcore.entity.User;
+import com.Auth.Authcore.repository.RegisterRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class LoginService
 {
+    @Autowired
+    private RegisterRepo repo;
+    public String registerUser(User user)
+    {
+        if(repo.existsByEmail(user.getEmail())){
+            return "Email is Already Exist";
+        }
+        repo.save(user);
+        return "Registration Successsfully";
+    }
+
 }
--- src/main/java/com/Auth/Authcore/repository/RegisterRepo.java
+++ src/main/java/com/Auth/Authcore/repository/RegisterRepo.java
@@ -3,14 +3,15 @@ package com.Auth.Authcore.repository;
 import com.Auth.Authcore.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface RegisterRepo extends JpaRepository<User, Long>
 {
 
-    List<User> findByName(String name);
-    List<User>findByEmail(String email);
+    Optional<User> findByUsername(String username);
 
-    List<User> findByPassword(String password);
+    Optional<User> findByEmail(String email);
+
     boolean existsByEmail(String email);
+
 }
